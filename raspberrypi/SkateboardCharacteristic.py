efc75069580e47a176dd46a59703346a673e00cc
Soravis Prakkamakul <asunnotthesun@gmail.com>
2019-03-14 21:18:16 -0700

    fix 1

--- raspberrypi/SkateboardCharacteristic.py
+++ raspberrypi/SkateboardCharacteristic.py
@@ -26,6 +26,7 @@ class SkateboardCharacteristic(Characteristic):
         
         # hardcoded
         print('SkateboardCharacteristic - %s - onWriteRequest: value = %s' % (self['uuid'], [hex(c) for c in self._value]))
+        print('data:', data)
         self.appState.direction = data
 
         if self._updateValueCallback:
