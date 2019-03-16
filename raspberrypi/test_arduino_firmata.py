07b99d3f52acbabcaf3072530e6c20d1890d99e7
Soravis Prakkamakul <asunnotthesun@gmail.com>
2019-03-16 20:12:20 +0000

    revise accelerometer mapping

--- raspberrypi/test_arduino_firmata.py
+++ raspberrypi/test_arduino_firmata.py
@@ -5,8 +5,16 @@ board = Arduino('/dev/ttyACM0')
 it = util.Iterator(board)
 it.start()
 board.analog[0].enable_reporting()
+board.analog[1].enable_reporting()
+board.analog[2].enable_reporting()
 while True:
-	value = board.analog[0].read()
-	print(value)
+	xr = board.analog[0].read()
+	yr = board.analog[1].read()
+	zr = board.analog[2].read()
+	if (xr is not None) and (yr is not None) and (zr is not None):
+		xg = (xr - 0.5) * 3.0
+		yg = (yr - 0.5) * 3.0
+		zg = (zr - 0.5) * 3.0
+		print(xg, yg, zg)
 	board.pass_time(0.5)
 	time.sleep(0.5)
